eee0c18922e1d4b90e358a01b65e9fd04f359815
BexCTDev <developer@bexct.com>
2017-02-21 15:20:01 -0500

    Home Page Updates

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>TestOne</title>
-</head>
-<body>
-<h1>Test one</h1>
-<p>Welcome to TestOne</p>
-</body>
+	<head>
+		<title>TestOne</title>
+	</head>
+	<body>
+		<h1 id="headline">Test one</h1>
+		<p id="maintext">Welcome to TestOne</p>
+		<button id="btn">Add new Line</button>
+		<ul id="list">
+			<li>First Item</li>
+			<li>Second Item</li>
+			<li>Third Item</li>
+			<li>Fourth Item</li>
+			<li>Fifth Item</li>
+		</ul>
+	</body>	
+	<script src="./main.js"></script>
 </html>
